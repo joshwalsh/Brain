9e50b8290c963b96018a5833adf1a878a56a9a7d
Josh Walsh <jwalsh@designinginteractive.com>
2014-05-10 19:25:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brain::Application.config.secret_key_base = '6b4e87785f6718bb78187a501f385fde8b419d16a8d05529057e7c8749163a64fe35b3e0cc2a54cc05d2c539ef7909317ef2cc977b2dfa71180db1c861d26b6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brain::Application.config.session_store :cookie_store, key: '_Brain_session'
